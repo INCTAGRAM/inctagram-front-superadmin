6b2fba2d86f251fafd0d9e819eac4e865fd2083d
Daniil Fefelov <fefelov.danil@gmail.com>
2023-06-28 12:48:41 +0400

    fix: change graphql url in apolloClient

--- src/helpers/apollo-client.ts
+++ src/helpers/apollo-client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const apolloClient = new ApolloClient({
-  uri: 'https://rickandmortyapi.com/graphql/',
+  uri: 'http://34.118.88.29:7000/graphql',
   cache: new InMemoryCache(),
 })
